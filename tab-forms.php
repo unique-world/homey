f7f4b7798ece7f20c40d7a03d84f1ebd42c94b6f
Frank <idalerali.com>
2023-06-18 15:54:36 +0500

    latest update

--- tab-forms.php
+++ tab-forms.php
@@ -1,4 +1,3 @@
-<link rel="stylesheet" href="assets/css/my-css.css">
 
 <form action="/submit-form" method="POST">
     <div class="form-tabs">
@@ -19,23 +18,23 @@
         <div class="tab-content">
 
 
-        <div id="tab_info" class="tab-pane active" style="width:45%;padding:10px 30px 0 0;float:left; margin-left:20px;">
-                            
-                            
-                            <label for="bedrooms" class="feature-label">Number of Bedrooms:</label><br>
-                            <input type="text" id="bedrooms" name="bedrooms" class="regular-text" placeholder="Enter number of bedrooms"><br><br>
-                            <label for="beds">Number of Beds:</label> <br>
+                <div id="tab_info" class="tab-pane active" >
+                                    
+                    <div style="width:35%;padding:10px 30px 0 0;float:left; margin-left:20px;">         
+                                    <label for="bedrooms" class="feature-label">Number of Bedrooms:</label><br>
+                                    <input type="text" id="bedrooms" name="bedrooms" class="regular-text" placeholder="Enter number of bedrooms"><br><br>
+                                    <label for="beds">Number of Beds:</label> <br>
 
-                            <input type="text" id="beds" name="beds" placeholder="Enter number of beds" class="regular-text"><br><br>
-                            <label for="size">Size:</label> <br>
-                            <input type="text" id="size" name="size" placeholder="Enter size" class="regular-text"><br><br>
-                            <label for="rooms">Number of Rooms:</label><br>
-                            <input type="text" id="rooms" name="rooms" placeholder="Enter number of rooms" class="regular-text"><br><br>
-                            <label for="guests">Affiliated booking links</label><br>
-                                <input type="text" id="guests" name="guests" placeholder="Enter Affiliated booking links" class="regular-text"  ><br><br>
-                                
-                   
-          </div>
+                                    <input type="text" id="beds" name="beds" placeholder="Enter number of beds" class="regular-text"><br><br>
+                                    <label for="size">Size:</label> <br>
+                                    <input type="text" id="size" name="size" placeholder="Enter size" class="regular-text"><br><br>
+                                    <label for="rooms">Number of Rooms:</label><br>
+                                    <input type="text" id="rooms" name="rooms" placeholder="Enter number of rooms" class="regular-text"><br><br>
+                                    <label for="guests">Affiliated booking links</label><br>
+                                        <input type="text" id="guests" name="guests" placeholder="Enter Affiliated booking links" class="regular-text"  ><br><br>
+                                        
+                        
+                </div>
 
           <div>
                     <label for="guests">Number of Guests:</label><br>
@@ -53,14 +52,14 @@
                   
 
 
+            </div>
+
 </div>
 
 
 
 
-
-
-            <div id="tab_pricing" class="tab-pane">
+        <div id="tab_pricing" class="tab-pane">
                 
             <label for="booking" class="feature-label">Booking type:</label><br>
             <select id="booking" name="bookingtype" class="regular-text">
@@ -88,12 +87,13 @@
                             <!-- Add more options as needed -->
                         </select> <br><br>
             <hr><br><label> Long-Term Pricing </label> <hr>
-            
-                        <label> Weekend-7 + nights</label><br>
-                        <input type=text class="regular-text" placeholder="Enter the price for a single day">
 
-                        <label style="margin-left:20%;"> Monthly-30 + nights</label> 
-                        <input type=text class="regular-text" placeholder="Enter the price for a single day" >
+            
+                        <label> Weekend-7 + nights</label>
+                        <input type=text class="regular-text" placeholder="Enter the price for a single day" style ="width: 18%;">
+
+                        <label style="margin-left:9%;"> Monthly-30 + nights</label> 
+                        <input type=text class="regular-text" placeholder="Enter the price for a single day"style ="width: 18%;" >
 
                         <hr><br><label>Additional Cost</label> <hr>
                         <label class="feature-label">Allow additional Guest</label> <br>
@@ -104,23 +104,61 @@
                         <label for="featured-no">
                         <input type="radio" id="featured-no" name="featured" value="no"> No </label> 
 
-                        <label class="feature-label" style="margin-left:43%;">Additional Guest?</label> 
-                        <input type=text class="regular-text" placeholder="Enter the price for one add" style="margin-left:2%;">
-
-            </div>
-
-
-
+                        <label class="feature-label" style="margin-left:30%;">Additional Guest</label> 
+                        <input type=text class="regular-text" placeholder="Enter the price for one add" style="margin-left:2%; width: 18%; ">
+                        <label class="feature-label" style="margin-left:2%;">Number of Guests</label> 
+                        <input type=text class="regular-text" placeholder="Enter the number of additional guest" style="margin-left:2%; width: 18%;">
+</div>
 
 
 
+
+                        <div id="tab_image_gallary" class="tab-pane">
+                        <label class="feature-label" style="margin-left:4%;">Image Gallery</label>
+                                <div id="image-gallery">
+                                    <!-- Images will be displayed here -->
+                                </div>
+                                <input type="file" id="image-input" accept="image/*" style="display: none;">
+                                <input type="button" value="Add Media" onclick="chooseImage()">
+
+                        </div>
+
+                     <div id="tab_virtual_tour" class="tab-pane">
+                         <label class="feature-label" style="margin-left:4%;">Virtual Tour</label> 
+                        <input type=text class="regular-text" placeholder="Enter virtual tour iframe/embedded code">
+                    </div>
+                   
+
+                    <div id="tab_video" class="tab-pane">
+                         <label class="feature-label" style="margin-left:4%;">Video URL</label> 
+                        <input type=text class="regular-text" placeholder="Enter Video Link or URL">
+                    </div>
+                    
+                    <div id="tab_location" class="tab-pane">
+                         <label class="feature-label" style="margin-left:0%;">Do you want to show the listing map?</label> <br>
+                         <label for="featured-yes">
+                        <input type="radio" id="featured-yes" name="featured" value="show">  Show  </label>
+                        <label for="featured-no">
+                        <input type="radio" id="featured-no" name="featured" value="hide"> Hide </label>  <br> 
+                        <label class="feature-label" style="margin-left:0%;">Apt, Suit (Optional)</label> <br>
+                        <input type=text class="regular-text" placeholder="Ex. 123">
+                        <label class="feature-label" style="margin-left:4%;">Zip code</label> 
+                        <input type=text class="regular-text" placeholder="Zip Code"><br>
+
+                        <label class="feature-label" style="margin-left:0%;">Address</label> <br>
+                        <input type=text class="regular-text" placeholder="Enter the listing Address">
+
+                    
+                    </div><br>
+                    
+                    <div id="tab_bedroom" class="tab-pane">
+                         <label class="feature-label" style="margin-left:4%;">Bedrooms/label> 
+                        <input type=text class="regular-text" placeholder="Enter virtual tour iframe/embedded code">
+                    </div>
             <!-- Add more tab content divs for each tab -->
 
-
-
-
-
-        </div>
+       </div>
+     
     </div><br>
     <button type="submit">Submit</button>
 </form>
@@ -146,7 +184,29 @@
         // Hide all tab content except for the active tab on page load
         $(".tab-content .tab-pane").not(".active").hide();
     });
+
+    function chooseImage() {
+    var imageInput = document.getElementById("image-input");
+    imageInput.click();
+    imageInput.onchange = function(event) {
+        var file = event.target.files[0];
+        if (file) {
+            var reader = new FileReader();
+            reader.onload = function() {
+                var imageElement = document.createElement("img");
+                imageElement.src = reader.result;
+                document.getElementById("image-gallery").appendChild(imageElement);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+}
+
+  
+
 </script>
 
 
 //<?php echo "Daler" ?>
+
+
